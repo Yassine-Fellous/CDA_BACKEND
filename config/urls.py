e76cd800152a4c853938c066df9f1942aaf3bcb7
yassine fellous <kenshi@MacBook-Air-de-yassine.local>
2025-07-11 00:39:15 +0200

    🚀 Test pour Railway : ajout double mapping des routes API

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,15 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Bienvenue sur l'API CDA Backend !")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('api/v1/', include('installations.urls')),
-    path('', include('installations.urls')) # Test URL for installations
+    path('', include('installations.urls')), # Test URL for installations
+    path('', home),  # Test URL for home
 ]
 #path('', include('api.urls', namespace='api'))
