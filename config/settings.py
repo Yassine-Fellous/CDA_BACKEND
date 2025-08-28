2eb30968b3e056768147dd5181b852fc338580cf
yassine fellous <kenshi@MacBook-Air-de-yassine.local>
2025-08-28 17:03:10 +0200

    fix: mise à jour settings.py

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://cdafrontend-production.up.railway.app"
+    "https://cdafrontend-production.up.railway.app",
 ]
 
 ROOT_URLCONF = 'config.urls'
