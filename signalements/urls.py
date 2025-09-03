1bd167e8f81d57459c3bdbfacaf18548683dfe09
yassine fellous <kenshi@MacBook-Air-de-yassine.local>
2025-09-03 03:51:53 +0200

    fix signalements

--- signalements/urls.py
+++ signalements/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_signalement, name='create_signalement'),
-    path('', views.list_signalements, name='list_signalements'),  # Optionnel
+    path('create/', views.create_signalement, name='create_signalement'),  # Optionnel
 ]
